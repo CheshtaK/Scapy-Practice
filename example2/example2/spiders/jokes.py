53bf7caabebdd162388c636d2b6997e31b723635
CheshtaK <32400008+CheshtaK@users.noreply.github.com>
2020-01-11 01:52:01 +0530

    scraping clean text

--- example2/example2/spiders/jokes.py
+++ example2/example2/spiders/jokes.py
@@ -1,4 +1,6 @@
 import scrapy
+from example2.items import JokeItem
+from scrapy.loader import ItemLoader
 
 class JokesSpider(scrapy.Spider):
     name = 'jokes'
@@ -9,10 +11,11 @@ class JokesSpider(scrapy.Spider):
 
     def parse(self, response):
         for joke in response.xpath("//div[@class='jokes']"):
-            yield {
-                'joke_text': joke.xpath(".//div[@class='joke-text']/p").extract_first()
 
-            }
+            l = ItemLoader(item=JokeItem(), selector=joke)
+            l.add_xpath('joke_text', ".//div[@class='joke-text']/p")
+
+            yield l.load_item()
 
         next_page = response.xpath("//li[@class='next']/a/@href").extract_first()
         if next_page is not None:
